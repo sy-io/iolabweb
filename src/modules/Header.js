54129a07721e64564e0706dcc10aeb800004cb64
gounbeee <gounbeee@gmail.com>
2022-12-21 21:20:44 +0900

    THIRD

--- src/modules/Header.js
+++ src/modules/Header.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import Box from '@material-ui/core/Box';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import { createTheme } from "@material-ui/core/styles";
-import { ThemeProvider } from "@material-ui/styles";
-import { red } from "@material-ui/core/colors";
+import Box from '@mui/material/Box';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import { makeStyles }  from "@mui/styles";
 
 import ScrollToButton from "./ScrollToButton";
 
